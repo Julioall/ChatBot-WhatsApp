87f68c39a61aefb71334064f77be079acb263d84
Julio Alves <95106201+Julioall@users.noreply.github.com>
2023-12-14 22:41:05 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-ACCOUNT_SID = "Seu sid da conta twilio"
-AUTH_TOKEN = "Seu token da conta"
-PHONE_TWILIO = "Seu numero Twilio"
-INICIAR_BOT = "join cent-automobile"
-DESTINATARIO = "Seu numero aqui" #+55998021488
+ACCOUNT_SID = "Seu sid da conta twilio" #Fica na sua conta do Twilio nesse link https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1
+AUTH_TOKEN = "Seu token da conta" #Fica na sua conta do Twilio nesse link https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1
+PHONE_TWILIO = "Seu numero Twilio" #Fica na sua conta do Twilio nesse link https://console.twilio.com/?frameUrl=%2Fconsole%3Fx-target-region%3Dus1
+INICIAR_BOT = "join cent-automobile" #Fica na sua conta do Twilio nesse link https://console.twilio.com/us1/develop/sms/try-it-out/whatsapp-learn?frameUrl=%2Fconsole%2Fsms%2Fwhatsapp%2Flearn%3Fx-target-region%3Dus1
+DESTINATARIO = "Seu numero aqui" #Use o formato "+556299921488"
